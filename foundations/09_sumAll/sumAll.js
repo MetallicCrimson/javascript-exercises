66fe869b46cc966a2a848eb9654368bd10620b1c
MetallicCrimson <tamerlein667@gmail.com>
2026-02-01 23:59:06 +0100

    Solve foundations/09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    if (!(typeof(a) == "number" && typeof(b) == "number" && parseInt(a) === a &&
+            parseInt(b) === b && a > 0 && b > 0)) {
+        return "ERROR";
+    }
 
+    if (b < a) {
+        let c = a;
+        a = b;
+        b = c;
+    }
+
+    return ((a+b) * (b-a+1))/2
+    
 };
 
 // Do not edit below this line
