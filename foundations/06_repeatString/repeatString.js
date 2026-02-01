39837a2c611d57124c2e616662c06edfae052d0b
MetallicCrimson <tamerlein667@gmail.com>
2026-02-01 23:36:40 +0100

    Solve foundations/06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(s, n) {
+    acc = ""
+    
+    if (n < 0) {
+        return "ERROR";
+    }
 
+    for (let i = 0; i < n; i++) {
+        acc += s;
+    }
+
+    return acc;
 };
 
 // Do not edit below this line
