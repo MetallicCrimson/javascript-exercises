c46ffd5fcaeb4efe8370acb3404a54ff325998a4
MetallicCrimson <tamerlein667@gmail.com>
2026-02-01 23:39:04 +0100

    Solve foundations/07

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(s) {
+    let acc = "";
 
+    for (let i = s.length-1; i >= 0; i--) {
+        acc += s[i];
+    }
+
+    return acc;
 };
 
 // Do not edit below this line
