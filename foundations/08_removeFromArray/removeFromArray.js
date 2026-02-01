a6bb078193805e82a36e38c94ea22ff8d4f4cae9
MetallicCrimson <tamerlein667@gmail.com>
2026-02-01 23:51:46 +0100

    Solve foundations/08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...deletables) {
+    console.log(deletables);
+    arr = arr.filter(a => !deletables.includes(a));
+
+    console.log(arr);
+    return arr;
 };
 
 // Do not edit below this line
