e221c018c49dca118d000e1d4f1f35118245f37f
MetallicCrimson <tamerlein667@gmail.com>
2026-02-02 00:17:59 +0100

    Solve foundations/11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  // return ((fahr - 32) / (5/9)).toFixed(1);
+  return Math.round(((fahr - 32) * (5/9)) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+  // return (cels * (9/5) + 32).toFixed(1);
+  return Math.round((cels * (9/5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
