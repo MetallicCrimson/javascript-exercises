8d0df7dac2fe2ebdaaf89305cb62ff4be0100e42
MetallicCrimson <tamerlein667@gmail.com>
2026-03-05 23:12:24 +0100

    Solve foundations/13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+const palindromes = function (s) {
+    let start = 0;
+    let end = s.length - 1;
 
+    while (start < end) {
+        while (s[start].match(/[A-Za-z0-9]/) === null) {
+            start++;
+        }
+
+        while (s[end].match(/[A-Za-z0-9]/) === null) {
+            end--;
+        }
+
+        if (s[start].toLowerCase() !== s[end].toLowerCase()) {
+            return false;
+        }
+
+        start++;
+        end--;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
