3d17eb07c6138a29dd5a77bc4c4742d30897c351
MetallicCrimson <tamerlein667@gmail.com>
2026-03-05 23:40:51 +0100

    Solve foundations/16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    arr.sort((a,b) => {
+        let yearFromA = a.yearOfDeath || new Date().getFullYear();
+        let yearFromB = b.yearOfDeath || new Date().getFullYear();
 
+        return (yearFromA-a.yearOfBirth) - (yearFromB-b.yearOfBirth);
+    });
+
+    return arr.at(-1);
 };
 
 // Do not edit below this line
