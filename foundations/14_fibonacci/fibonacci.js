7e13b5a8a820b1111bdc85fdd17f4ab6ee78c8c3
MetallicCrimson <tamerlein667@gmail.com>
2026-03-05 23:27:07 +0100

    Solve foundations/14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = Number(n);
 
+    if (n === 0) {
+        return 0;
+    } else if (n < 0) {
+        return "OOPS";
+    }
+
+
+    let sequence = [1, 1];
+
+    for (let i = 2; i < n; i++) {
+        sequence.push(sequence[i-1] + sequence[i-2]);
+    }
+
+    return sequence[n-1];
 };
 
 // Do not edit below this line
